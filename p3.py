4d65e2a32d4be1c423ccae9e2e99b11545ee77da
Berkay <146880763+PBeekay@users.noreply.github.com>
2025-08-25 16:39:20 +0300

    Update p3.py

--- p3.py
+++ p3.py
@@ -1,15 +1,16 @@
-import math
+def en_buyuk_asal_carpan(n):
+    i = 2
 
-def largest_prime_factor(n: int) -> int:
-    """Return the largest prime factor of n."""
-    while n % 2 == 0:
-        n //= 2
-    f = 3
-    while f * f <= n:
-        if n % f == 0:
-            n //= f
+    while i * i <= n:
+        if n % i:
+            # Eğer n, i'ye bölünmüyorsa, bir sonraki sayıya geç.
+            i += 1
+        else:
+            # Eğer n, i'ye bölünüyorsa, n'i i'ye bölerek küçült.
+            n //= i
     return n
 
-if __name__ == "__main__":
-    num = 600851475143
-    print(largest_prime_factor(num))  # -> 6857
+sayi = 600851475143
+
+sonuc = en_buyuk_asal_carpan(sayi)
+print(f"{sayi} sayısının en büyük asal çarpanı: {sonuc}")
